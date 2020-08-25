ba2e552518288a26a8967b9d418b4ccc32e9d1b1
ummberto88@gmail.com <ummberto88@gmail.com>
2020-08-25 19:14:52 +0700

    Do task Range

---
+++ RangeTask/Range.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeTask
+{
+    class Range
+    {
+        public double From { get; set; }
+
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            double epsilon = 1.0e-10;
+
+            return (number - From >= -epsilon) && (To - number >= -epsilon);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Непрерывный вещественный числовой диапазон на прямой лежит в границах от " + From + " до " + To);
+        }
+    }
+}
---
+++ RangeTask/RangeMain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeTask
+{
+    class RangeMain
+    {
+        public static double PrintAndRead(string message)
+        {
+            Console.Write(message);
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static void Main()
+        {
+            double from = PrintAndRead("Введите начало диапазона: ");
+
+            double to;
+
+            for (; ; )
+            {
+                to = PrintAndRead("Введите конец диапазона: ");
+
+                double epsilon = 1.0e-10;
+
+                if (to - from > epsilon)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Введите корректный конец диапазона!");
+            }
+
+            Range range = new Range(from, to);
+            range.Print();
+
+            double rangeLength = range.GetLength();
+            Console.WriteLine("Длина диапазона равна: " + rangeLength);
+
+            double number = PrintAndRead("Введите любое число, для проверки принадлежности его к данному диапазону: ");
+
+            if (range.IsInside(number))
+            {
+                Console.WriteLine("Это число лежит в данном диапазоне!");
+            }
+            else
+            {
+                Console.WriteLine("Это число не входит в данный диапазон!");
+            }
+        }
+    }
+}
